56376e9745a0e650fae7bc20a41a689aaca9de61
yumao12 <71052683+yumao12@users.noreply.github.com>
2020-10-08 11:19:39 +0800

    Add files via upload

---
+++ Upload.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using Renci.SshNet;
+
+namespace DAL
+{
+    public class Upload
+    {
+        public Upload() { }
+        private static Upload _instance = new Upload();
+        public static Upload Instance {
+            get {
+                return _instance;
+            }
+        }
+        public string UpImg(IFormFile file, string filename) {
+            if (file == null || file.ContentType.StartsWith("image"))
+                return null;
+            else {
+                string ext = Path.GetExtension(file.FileName);
+                filename = $"{filename}{ext}";
+                using (FileStream fs = File.Create(filename)) {
+                    file.CopyTo(fs);
+                    fs.Flush();
+                }
+                return ext;
+            }
+        }
+        }
+    }
+
